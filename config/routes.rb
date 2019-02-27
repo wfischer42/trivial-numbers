531b44366f6968a1534f64f8c816ce24d4660559
William Fischer <william.fischer42@gmail.com>
2019-02-27 07:28:54 -0700

    Add basic styling from wireframe

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/', to: 'game#question'
-  post '/', to: 'game#answer'
-  post '/newgame', to: 'game#new'
+  put '/', to: 'game#answer'
+  post '/', to: 'game#new'
 end
